5fcc67e825788e667329c1177f1c5edd501b4f6c
austynbenyak <abenyak@kent.edu>
2019-02-26 23:20:58 -0500

    Dropped in

---
+++ script.js
@@ -0,0 +1,21 @@
+var navTrigger = document.getElementById('nav-trigger');
+var nav = document.getElementById('nav');
+var labels = document.getElementsByClassName('nav-label');
+
+// Event Listening
+navTrigger.addEventListener('click', navToggle);
+window.addEventListener('resize', resize);
+
+
+function navToggle(e) {
+  var closed = (navTrigger.className.indexOf('close') > 0);
+  if(closed) {
+    navTrigger.className = 'nav-trigger open';
+    nav.className = 'out';
+  } else {
+    navTrigger.className = 'nav-trigger close';
+    nav.className = 'in';
+  }
+}
+
+window.onload = resize;
